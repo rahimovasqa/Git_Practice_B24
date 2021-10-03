ddf5b817ab2193a3151571b707ce59735359a0b9
rahimova <rahimovasqa@gmail.com>
2021-10-03 12:48:21 -0400

    updated the third file again

--- src/setup/Third.java
+++ src/setup/Third.java
@@ -5,5 +5,6 @@ public class Third {
     public static void main(String[] args) {
         System.out.println("Hello Universe");
         System.out.println("Happy Sunday");
+        System.out.println("Have fun with Git");
     }
 }
